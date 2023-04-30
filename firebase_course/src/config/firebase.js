93bc69702c509ed2584f7e4d9c7e36452506f48d
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-04-30 20:10:56 +0100

    Setting up firebase

---
+++ firebase_course/src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuP441ljp_jEliqGG73XXJfbHaQjKWBr4",
+  authDomain: "fir-course-d3af6.firebaseapp.com",
+  projectId: "fir-course-d3af6",
+  storageBucket: "fir-course-d3af6.appspot.com",
+  messagingSenderId: "496793473512",
+  appId: "1:496793473512:web:163df5949a1e863d4dd45d"
+};
+
+const app = initializeApp(firebaseConfig);
