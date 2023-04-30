c4f8ecd6ac80ec8e5ecd4f7920d7a3e1bcd8498b
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-04-30 22:38:44 +0100

    Reading data from the firestore

--- firebase_course/src/App.jsx
+++ firebase_course/src/App.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Auth } from './components/index';
+import { db } from './config/firebase';
+import { getDocs, collection } from 'firebase/firestore';
 import './App.css';
 
 
 
 const App = () => {
+  const [movieList, setMovieList] = useState([]);
+
+  const movieListRef = collection(db, "movies");
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      try {
+        const data = await getDocs(movieListRef);
+        const filteredData = data.docs.map((doc) => (
+          { ...doc.data(), id: doc.id }
+        ));
+        setMovieList(filteredData);
+      }
+      catch(err) {
+        console.error(err);
+      }
+    };
+
+    getMovieList();
+  }, []); 
+
   return (
     <div className='app'>
       <Auth />
+      <div style={{ 
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '2rem',
+      }}>
+        {
+          movieList.map(movie => (
+            <div key={ movie.id }>
+              <h2 style={{ color: movie.hasOscar ? 'greenyellow' : 'red' }}>{ movie.title }</h2>
+              <p>Date: <b>{ movie.releaseDate }</b></p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 };
--- firebase_course/src/config/firebase.js
+++ firebase_course/src/config/firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
+
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCuP441ljp_jEliqGG73XXJfbHaQjKWBr4",
@@ -11,5 +14,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
